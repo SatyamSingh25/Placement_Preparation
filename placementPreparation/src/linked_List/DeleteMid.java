ccf52e58bd332713cb64ec6cafe42e00d210ec0f
Satyam Singh <satyamsinghkv2@gmail.com>
2021-05-11 17:20:06 +0530

    Added DeleteMid in LL

---
+++ placementPreparation/src/linked_List/DeleteMid.java
@@ -0,0 +1,28 @@
+package linked_List;
+/*Question Link  : https://www.geeksforgeeks.org/delete-middle-of-linked-list/
+ * Difficulty : Easy
+ */
+public class DeleteMid {
+	
+	Node deleteMid(Node head) {
+        Node slowPtr = head;
+        Node fastPtr = head;
+        Node prevPtr = slowPtr;
+        
+        if(head!=null){
+            while(fastPtr!=null && fastPtr.next!=null){
+                prevPtr = slowPtr;
+                slowPtr = slowPtr.next;
+                fastPtr = fastPtr.next.next;
+            }
+        }
+        
+        if(slowPtr==prevPtr){
+            return null;
+        }
+        prevPtr.next = slowPtr.next;
+        
+        return head;
+    }
+
+}
