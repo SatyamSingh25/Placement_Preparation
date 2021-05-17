53b00f1bf02393d53f34a6a2ee889ca19af9cc9c
Satyam Singh <satyamsinghkv2@gmail.com>
2021-05-17 23:21:39 +0530

    Created Binary tree

---
+++ placementPreparation/src/binary_Tree/Create_Tree.java
@@ -0,0 +1,48 @@
+package binary_Tree;
+
+import java.util.*;
+
+class Node{
+	int data;
+	Node left, right;
+	
+	Node(){}
+	
+	Node(int d){
+		this.data = d;
+		this.left = null;
+		this.right = null;
+	}
+	Node(int d, Node left, Node right){
+		this.data = d;
+		this.left = left;
+		this.right = left;
+	}
+}
+public class Create_Tree {
+	public static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		Node root = createTree();
+	}
+	
+	public static Node createTree() {
+		Node root = null;
+		System.out.println("Enter root data : ");
+		int data = sc.nextInt();
+		
+		if(data == -1) {
+			return null;
+		}
+		
+		root = new Node(data);
+		System.out.println("enter data of left Node : ");
+		root.left = createTree();
+		
+		System.out.println("Enter data of right Node : ");
+		root.right = createTree();
+		
+		return root;
+	}
+
+}
