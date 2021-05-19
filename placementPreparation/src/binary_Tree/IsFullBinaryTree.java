5a5b6d10407c513b49fd0e7d5f04992d212ae793
Satyam Singh <satyamsinghkv2@gmail.com>
2021-05-19 11:02:09 +0530

    check tree if full B-tree or not

--- placementPreparation/src/binary_Tree/IsFullBinaryTree.java
+++ placementPreparation/src/binary_Tree/IsFullBinaryTree.java
@@ -1,5 +1,7 @@
 package binary_Tree;
-
+/* Problem description : https://practice.geeksforgeeks.org/problems/full-binary-tree/1/?track=amazon-trees&batchId=192#
+ * Difficulty : Medium 
+ */
 public class IsFullBinaryTree {
 	public boolean isFullTree(Node root) {
 		if(root == null)
