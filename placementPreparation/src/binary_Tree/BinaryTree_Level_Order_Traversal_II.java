bf026b56d9e3b3be4ca102408d3fe68e53150262
Satyam Singh <satyamsinghkv2@gmail.com>
2021-05-27 22:35:37 +0530

    Level order traversal II LEETCODE 107

---
+++ placementPreparation/src/binary_Tree/BinaryTree_Level_Order_Traversal_II.java
@@ -0,0 +1,67 @@
+package binary_Tree;
+import java.util.*;
+/**
+ * @author Satyam
+ *                               LEETCODE 107
+ * Problem link : https://leetcode.com/problems/binary-tree-level-order-traversal-ii/
+ * Description  : Given the root of a binary tree, return the bottom-up level order
+ *                traversal of its nodes' values. (i.e., from left to right, level by
+ *                level from leaf to root).
+ *                
+ *               
+ * Tree is like : (3)
+ *               /   \
+ *              (9)  (20)
+ *                   /  \
+ *                 (15)  (7)
+ *                 
+ * input : root = [3,9,20,null,null,15,7]
+ * Output :  [[15,7],[9,20],[3]]
+ * 
+ * Difficulty : Medium
+ */
+public class BinaryTree_Level_Order_Traversal_II {
+	/**
+	 * Definition for a binary tree node.
+	 * public class Node {
+	 *     int data;
+	 *     TreeNode left;
+	 *     TreeNode right;
+	 *     TreeNode() {}
+	 *     TreeNode(int data) { this.data = data; }
+	 *     TreeNode(int data, TreeNode left, TreeNode right) {
+	 *         this.data = data;
+	 *         this.left = left;
+	 *         this.right = right;
+	 *     }
+	 * }
+	 */
+	
+	//approach is similar as Level order traversal
+	public List<List<Integer>> levelOrderBottom(Node root) {
+        if(root == null)
+            return new LinkedList<List<Integer>>();
+        
+        List<List<Integer>> arr = new LinkedList<List<Integer>>();
+        Queue<Node> queue = new LinkedList<Node>();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            ArrayList<Integer> innerList = new ArrayList<Integer>();
+            
+            for(int i=0; i<size; i++){
+                Node node = queue.poll();
+                innerList.add(node.data);
+                
+                if(node.left != null)
+                    queue.add(node.left);
+                
+                if(node.right != null)
+                    queue.add(node.right);
+            }
+            arr.add(innerList);
+        }
+        Collections.reverse(arr);
+        return arr;
+    }
+}
