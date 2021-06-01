73be7923ae62b39c9e1ddf1ca7be35da0598cf9b
Satyam Singh <satyamsinghkv2@gmail.com>
2021-06-02 00:26:43 +0530

    Maximum Subarray LEECODE 58

---
+++ placementPreparation/src/dynamic_Programming/MaximumSubArray.java
@@ -0,0 +1,26 @@
+package dynamic_Programming;
+/**
+ * @author Satyam Singh
+ * Problem link : https://leetcode.com/problems/maximum-subarray/
+ * 
+ * input : [-2,1,-3,4,-1,2,1,-5,4]
+ * output : 6
+ * Explanation: [4,-1,2,1] has the largest sum = 6.
+ *  
+ * Difficulty: Easy
+ */
+public class MaximumSubArray {
+	
+	public int maxSubArray(int[] nums) {
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i<nums.length; i++){
+            sum = sum + nums[i];
+            max = Math.max(sum, max);
+            if(sum < 0){
+                sum = 0;
+            }
+        }
+        return max;
+    }
+}
