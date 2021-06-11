75962cdde6f274b7b332fa07e271306fbbd7a517
Satyam Singh <satyamsinghkv2@gmail.com>
2021-06-11 23:50:44 +0530

    minimum path from dp[0][0] to dp[n][n]

---
+++ placementPreparation/src/dynamic_Programming/MinimumPathFrom_Start_to_END.java
@@ -0,0 +1,42 @@
+package dynamic_Programming;
+/**
+ * @contributed_By Satyam Singh
+ * -------------------------------LEETCODE 64--------------------------------------------   
+ * Problem link : https://leetcode.com/problems/minimum-path-sum/
+ * description : Given a m x n grid filled with non-negative numbers,
+ *               find a path from top left to bottom right, which minimizes
+ *               the sum of all numbers along its path.
+
+                 Note: You can only move either down or right at any point in time.
+   
+   Input: grid = [[1,3,1],[1,5,1],[4,2,1]]
+   Output: 7
+   Explanation: Because the path 1 → 3 → 1 → 1 → 1 minimizes the sum.
+   
+   Difficulty : Medium
+ */
+public class MinimumPathFrom_Start_to_END {
+	//grid traversing
+	public int minPathSum(int[][] grid) {
+		if(grid == null || grid.length==0) {
+			return 0;
+		}
+		
+		int[][] dp = new int[grid.length][grid[0].length];
+		for(int i=0; i<dp.length; i++) {
+			for(int j=0; j<dp[0].length; j++) {
+				dp[i][j] += grid[i][j];
+				if(i>0 && j>0) {
+					dp[i][j] += Math.min(dp[i-1][j], dp[i][j-1]);
+				}
+				else if(i>0) {
+					dp[i][j] += dp[i-1][j];
+				}
+				else if(j>0) {
+					dp[i][j] += dp[i][j-1];
+				}
+			}
+		}
+		return dp[dp.length][dp[0].length];
+	}
+}
