2d3d5c60bd65d84e585ee84ade94771149bacaa4
Satyam Singh <satyamsinghkv2@gmail.com>
2021-06-27 18:35:35 +0530

    Group of anagrams

--- placementPreparation/src/array/GroupAnagrams.java
+++ placementPreparation/src/array/GroupAnagrams.java
@@ -10,21 +10,28 @@ import java.util.*;
 public class GroupAnagrams {
 	//using hash map to solve this problem.
 	public List<List<String>> groupAnagrams(String[] strs) {
+		
         HashMap<String, ArrayList<String>> map = new HashMap<>();
+        
         for(String i: strs){
+        	
             char[] tempStr = i.toCharArray();
             Arrays.sort(tempStr);
+            
             String tempString  = String.valueOf(tempStr);
             
             if(!map.containsKey(tempString)){
                 map.put(tempString, new ArrayList<String>());
             }
+            
             map.get(tempString).add(i);
         }
         List<List<String>> res = new ArrayList<>();
+        
         for(ArrayList<String> itr: map.values()){
             res.add(itr);
         }
+        
         return res;
     }
 }
