233f5dc4076177f3a0b65fbe4b14e3b4385e4eba
Satyam Singh <satyamsinghkv2@gmail.com>
2021-06-29 12:48:28 +0530

    case 2

---
+++ placementPreparation/src/exceptionHandling/Case2.java
@@ -0,0 +1,13 @@
+package exceptionHandling;
+/**
+ * @Contributor Satyam Singh
+ * @exception_Handling
+ */
+public class Case2 extends Throwable{
+	/*If Case2 class doesn't inherit the Throwable || Exception || RuntineException
+	 *the compiler will give "incompatable type" error
+	 */
+	public static void main(String[] args) throws Case2{
+		
+	}
+}
