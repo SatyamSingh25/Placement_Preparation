d7e9298f69516696293ddb9ff2af55da2952c735
Satyam Singh <satyamsinghkv2@gmail.com>
2021-06-29 12:48:19 +0530

    case 1

---
+++ placementPreparation/src/exceptionHandling/Case1.java
@@ -0,0 +1,18 @@
+package exceptionHandling;
+/**
+ * @Contributed Satyam Singh
+ * 
+ * @for_class_throws_not_valid
+ */
+public class Case1 {
+	
+	//for constructor throws exception is valid
+	Case1() throws Exception{		
+	}
+
+	//for methods throws exception is valid
+	public static void main(String[] args) {
+		
+	}
+
+}
