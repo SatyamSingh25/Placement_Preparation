d9525a2003d0ac23dcacf1d230c3aef2c141fe97
Satyam Singh <satyamsinghkv2@gmail.com>
2021-07-01 23:34:41 +0530

    final Method/Class concept

--- placementPreparation/src/oops/FinalMthod.java
+++ placementPreparation/src/oops/FinalMthod.java
@@ -1,11 +1,13 @@
 package oops;
+//final Class can not inherited
 //final method can not override
-public class FinalMthod {
+public class FinalMthod extends Final2 {
 	final void m1() {
 		
 	}
 	void m1() {
 		
 	}
+	
 
 }
