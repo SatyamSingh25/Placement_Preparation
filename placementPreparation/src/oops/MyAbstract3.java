009723c80c17aecfa98c982e75cc33c1d8e70e62
Satyam Singh <satyamsinghkv2@gmail.com>
2021-07-02 22:45:29 +0530

    Abstract class with non-abstract method (true)

---
+++ placementPreparation/src/oops/MyAbstract3.java
@@ -0,0 +1,9 @@
+package oops;
+//an abstract class can have non abstract method
+
+public abstract class MyAbstract3 {
+	//non abstract method
+	void fun() {
+		System.out.println("NON-Abstract method");
+	}
+}
