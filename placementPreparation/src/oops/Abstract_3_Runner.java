7fa3fca48d8a0f224f59ad4a90658102e9eb2bbd
Satyam Singh <satyamsinghkv2@gmail.com>
2021-07-02 22:29:37 +0530

    Abstract 3

---
+++ placementPreparation/src/oops/Abstract_3_Runner.java
@@ -0,0 +1,10 @@
+package oops;
+
+public class Abstract_3_Runner extends MyAbstract3{
+
+	public static void main(String[] args) {
+		Abstract_3_Runner obj3 = new Abstract_3_Runner();
+		Abstract_3_Runner.fun();
+	}
+
+}
