a01ccbb39908855e33906b4ad90e18c3745d2a73
Satyam Singh <satyamsinghkv2@gmail.com>
2021-09-20 11:06:48 +0530

    try1

--- placementPreparation/src/array/TryGit.java
+++ placementPreparation/src/array/TryGit.java
@@ -4,7 +4,7 @@ public class TryGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello hg git");
+		System.out.println("helo hg git");
 	}
 	
 
