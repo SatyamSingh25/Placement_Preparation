a99a44a1c1e1a7d6d2bafc1dc5e42d49eb620c63
Satyam Singh <satyamsinghkv2@gmail.com>
2021-10-09 16:18:58 +0530

    Sort array without sorting (O(n^2))

---
+++ placementPreparation/src/array/SortArrayWithoutSorting.java
@@ -0,0 +1,21 @@
+package array;
+
+public class SortArrayWithoutSorting {
+
+	public static void main(String[] args) {
+		int[] arr = new int[] {4,2,5,1,7,4};
+		int n = arr.length;
+		for(int i=0; i<n; i++) {
+			for(int j=i+1; j<n; j++) {
+				if(arr[i] > arr[j]) {
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		for(int i: arr)
+			System.out.print(i+ " ");
+	}
+
+}
