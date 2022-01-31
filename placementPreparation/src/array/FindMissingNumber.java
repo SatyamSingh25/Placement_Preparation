f10ba46d248e3329f6e0e64f1e6513bb2ef7d20c
Satyam Singh <satyamsinghkv2@gmail.com>
2022-02-01 00:45:04 +0530

    finding missing number

--- placementPreparation/src/array/FindMissingNumber.java
+++ placementPreparation/src/array/FindMissingNumber.java
@@ -3,14 +3,15 @@ import java.util.*;
 public class FindMissingNumber {
 	//Approach 1 
 	public static int findMissing1(int[] arr) {
-		int res = (arr.length*(arr.length+1))/2;
+		int n  = arr.length;
+		int res = (n*(n+1))/2;
 		for(int i: arr) {
 			res-=i;
 		}
 		return res;
 	}
 	public static void main(String[] args) {
-		int[] arr = new int[] {0,1,3,4,5,8,6,7};
+		int[] arr = new int[] {0 ,1, 2, 4, 5, 6};
 		System.out.println(findMissing1(arr));
 	}
 }
