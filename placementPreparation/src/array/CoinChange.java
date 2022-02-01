60b632bbe4284a0c50ee12ce4a3c3e00aa932d27
Satyam Singh <satyamsinghkv2@gmail.com>
2022-02-01 15:19:34 +0530

    Coin Change [DP]

--- placementPreparation/src/array/CoinChange.java
+++ placementPreparation/src/array/CoinChange.java
@@ -4,6 +4,7 @@ import java.util.*;
  * @Contrinbuted Satyam Singh
  * Problem link : https://leetcode.com/problems/coin-change/
  * Difficulty : Medium
+ * tag: DP, Array
  */
 public class CoinChange {
 	//dynamic programming approach
@@ -11,18 +12,16 @@ public class CoinChange {
 		int[] dp = new int[amount+1];
 		Arrays.fill(dp, amount+1);
 		dp[0] = 0;
-		for(int i=0; i<=amount; i++) {
-			for(int c:coins) {
-				if(i>=c) {
-					dp[i] = Math.min(dp[i], dp[i-c]+1);
+		for(int i=1; i<=amount; i++) {
+			for(int coin: coins) {
+				if(i >= coin) {
+					dp[i] = Math.min(dp[i], dp[i-coin]+1);
 				}
 			}
 		}
-		if(dp[amount]>amount) {
-			return -1;
-		}
 		return dp[amount];
 	}
+	//Driver Code
 	public static void main(String[] args) {
 		int[] arr = new int[] {1,2,5};
 		System.out.println(coinChange(arr, 11));
