08ff2f528fbc191c7f73af79d2f4c024eaab61a4
Satyam Singh <satyamsinghkv2@gmail.com>
2022-02-10 20:13:25 +0530

    Rotate 90 Degree a matrix

---
+++ placementPreparation/src/matrix/RotateMatrix90Matrix_Anti_ClockWise.java
@@ -0,0 +1,61 @@
+package matrix;
+/**
+ * @ReverseRows_then_Transpose
+ * @Matrix
+ * @GFG @leetcode
+ */
+public class RotateMatrix90Matrix_Anti_ClockWise {
+	
+	public static int[][] rotate90DegreeClockWise(int[][] mat){
+		int row = mat.length;
+		int col = mat[0].length;
+
+		//reverse matrix row wise
+		for(int i=0; i<row; i++) {
+			int start = 0;
+			int end = mat[i].length-1;
+			while(start < end) {
+				int temp = mat[i][start];
+				mat[i][start] = mat[i][end];
+				mat[i][end] = temp;
+				start++;
+				end--;
+			}
+		}
+
+		//transposing matrix
+		for(int i=0; i<row; i++) {
+			for(int j=i+1; j<col; j++) {
+				int temp = mat[i][j];
+				mat[i][j] = mat[j][i];
+				mat[j][i] = temp;
+			}
+		}
+		return mat;
+	}
+	
+	public static void main(String[] args) {
+		
+		int[][] mat = new int[][] {{1,2,3},
+									{4,5,6},
+									{7,8,9}};
+		
+		System.out.println("Before rotation:");
+		for(int i=0; i<mat.length; i++) {
+			for(int j=0; j<mat[i].length; j++) {
+				System.out.print(mat[i][j]+" ");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("after rotation:");
+		mat = rotate90DegreeClockWise(mat);
+		for(int i=0; i<mat.length; i++) {
+			for(int j=0; j<mat[i].length; j++) {
+				System.out.print(mat[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
