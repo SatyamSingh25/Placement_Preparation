1cbc2f9dd4991483333f84c5b5a6891d3b016c9d
Satyam Singh <satyamsinghkv2@gmail.com>
2022-07-05 21:08:20 +0530

    Group of Anagrams

--- placementPreparation/src/array/GroupAnagrams.java
+++ placementPreparation/src/array/GroupAnagrams.java
@@ -8,17 +8,18 @@ import java.util.*;
  *	Difficulty : Medium
  */
 public class GroupAnagrams {
+	//Time: O(N * M * nLogN)
 	//using hash map to solve this problem.
 	public List<List<String>> groupAnagrams(String[] strs) {
 		
         HashMap<String, ArrayList<String>> map = new HashMap<>();
         
-        for(String i: strs){
+        for(String i: strs){ //O(N)
         	
-            char[] tempStr = i.toCharArray();
-            Arrays.sort(tempStr);
+            char[] tempStr = i.toCharArray(); //O(M)
+            Arrays.sort(tempStr); //NlogN
             
-            String tempString  = String.valueOf(tempStr);
+            String tempString  = new String(tempStr);
             
             if(!map.containsKey(tempString)){
                 map.put(tempString, new ArrayList<String>());
@@ -34,4 +35,38 @@ public class GroupAnagrams {
         
         return res;
     }
+	
+	//Approach 2
+	//Time: O(n * m)
+	public static List<List<String>> groupAnagrams2(String[] strs) {
+		
+        HashMap<String, ArrayList<String>> map = new HashMap<>();
+        
+        for(String i: strs){
+        	
+            char[] tempStr = new char[26];
+            
+            for(int ch: i.toCharArray()) {
+            	tempStr[ch-'a']++;
+            }
+            
+            String tempString  = String.valueOf(tempStr);
+            
+            if(!map.containsKey(tempString)){
+                map.put(tempString, new ArrayList<String>());
+            }
+            
+            map.get(tempString).add(i);
+        }
+        
+        List<List<String>> res = new ArrayList<>();        
+        res.addAll(map.values());
+        
+        return res;
+    }
+	
+	public static void main(String[] args) {
+		String[] words = new String[] {"eat","tea","tan","ate","nat","bat"};
+		System.out.print(groupAnagrams2(words));
+	}
 }
