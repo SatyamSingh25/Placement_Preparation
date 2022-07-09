ecc4231b4a34fdf5622c33510469ce9d29390069
Satyam Singh <satyamsinghkv2@gmail.com>
2022-07-09 22:41:24 +0530

    Static check

---
+++ placementPreparation/src/oops/Static1.java
@@ -0,0 +1,31 @@
+package oops;
+
+import java.util.ArrayList;
+
+class StaticDriver{
+	static int number = 0;
+}
+
+public class Static1{
+
+	public static void main(String[] args) {
+//		StaticDriver staticDriver = new StaticDriver();
+//		int x = staticDriver.number;
+//		System.out.println(number);
+//		
+//		int num2 = 1;
+//		double num1 = num2;
+		
+//		ArrayList<Integer> arr = new ArrayList<>();
+//		arr.add(1);
+//		arr.add(2);
+//		arr.add(3);
+//		arr.add(4);
+//		System.out.println(arr);
+//		arr.set(2,  6);
+//		System.out.println(arr);
+//		arr.add(1, 19);
+//		System.out.println(arr);
+	}
+
+}
