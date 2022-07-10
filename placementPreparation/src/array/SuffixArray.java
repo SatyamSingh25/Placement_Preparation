c1efd2cb3630075b572d26244426d432dfd040ad
Satyam Singh <satyamsinghkv2@gmail.com>
2022-07-11 01:33:02 +0530

    Suffix array of string

--- placementPreparation/src/array/SuffixArray.java
+++ placementPreparation/src/array/SuffixArray.java
@@ -1,20 +1,22 @@
 package array;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class SuffixArray {
-	public static int[] arr;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		StringBuilder str = new StringBuilder(sc.next());
 		int n = str.length();
-		String StringArray[] = new String[n-1];
-		arr = new int[n-1];
+		String[] StringArray = new String[n-1];
+		int[] arr = new int[n-1];
 		int x = 0;
 		for(int i=1; i<n; i++) {
 			StringArray[x] = str.substring(i);
 			arr[x] = i;
 			x++;
-		}	
+		}
+		System.out.println(Arrays.toString(StringArray));
+		System.out.println(Arrays.toString(arr));
 	}
 }
