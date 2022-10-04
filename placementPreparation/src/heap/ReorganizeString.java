a7df5314aedb1c45cd983dc647fafb47e1d4fbd5
Satyam Singh <satyamsinghkv2@gmail.com>
2022-10-04 23:14:50 +0530

    rearrange string as 2 adjacent doesn't have same character

--- placementPreparation/src/heap/ReorganizeString.java
+++ placementPreparation/src/heap/ReorganizeString.java
@@ -22,23 +22,31 @@ public class ReorganizeString {
 	public String reorganizeString(String s) {       
         HashMap<Character, Integer> map = new HashMap<Character, Integer>();
         
+        //frequencies of characters
         for(char c: s.toCharArray()){
-            map.put(c, map.getOrDefault(c,0)+1);
+            map.put(c, map.getOrDefault(c,0)+1); 
         }
         
+        //Max Heap according to frequency of characters
         PriorityQueue<Character> maxHeap = new PriorityQueue((a,b)->map.get(b)-map.get(a));
         maxHeap.addAll(map.keySet());
         
+        
         StringBuilder result = new StringBuilder();
         while(maxHeap.size() > 1){
-            char curr = maxHeap.remove();
-            char next = maxHeap.remove();
+            char curr = maxHeap.remove(); //removing top element
+            char next = maxHeap.remove(); //removing 2nd top element
             
+            //adding top two characters into result string because two adjacent doesn't to be same
             result.append(curr);
             result.append(next);
             
+            //decreasing the count by 1 of used characters
             map.put(curr, map.get(curr)-1);
             map.put(next, map.get(next)-1);
+            
+            //after removing -> if count of USED characters are not 0[ZERO] then 
+            //                  again add that in maxHeap (acc/to freq of characters)
             
             if(map.get(curr)>0){
                 maxHeap.add(curr);
@@ -48,6 +56,9 @@ public class ReorganizeString {
             }
         }
         
+        //if last element in the maxHeap have count>1 
+        //then it means -> two adjacent are going to be happen so RETURN "EMPTY STRING"
+        //otherwise add that into result string
         if(!maxHeap.isEmpty()){
             char last = maxHeap.remove();
             if(map.get(last)>1){
