4c530dd913d9800161261fe1652ed38a6fff92a2
Satyam Singh <satyamsinghkv2@gmail.com>
2023-08-03 18:15:54 +0530

    create all possible strings from mobile digit given

--- placementPreparation/src/backtrack_or_recursion/LetterCombination.java
+++ placementPreparation/src/backtrack_or_recursion/LetterCombination.java
@@ -1,5 +1,18 @@
 package backtrack_or_recursion;
 import java.util.*;
+/**
+ * @author Satyam
+ * -----------------------------------------Letter Combinations of a Phone Number-------------------------------------
+ * Given a string containing digits from 2-9 inclusive, 
+ * return all possible letter combinations that the number could represent. 
+ * Return the answer in any order.
+ * 
+ * A mapping of digits to letters (just like on the telephone buttons) is given below.
+ * Note that 1 does not map to any letters.
+ * 
+ * Input: digits = "23"
+ * Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+ */
 
 public class LetterCombination {
 	
@@ -16,14 +29,13 @@ public class LetterCombination {
 	}
 	
 	public static void solve(String digits, String[] alphas, int currIndex, String str){
+		
 		if(currIndex == digits.length()) {
-			System.out.println(++count);
 			combinations.add(str);
 			return;
 		}
 		
 		String possibleAplhas = alphas[digits.charAt(currIndex) - '0'];
-		System.out.println(possibleAplhas);
 		for(char ch: possibleAplhas.toCharArray()) {
 			solve(digits, alphas, currIndex + 1, str + ch);
 		}
@@ -32,6 +44,8 @@ public class LetterCombination {
 	public static void main(String[] args) {
 		String digits = "232";
 		letterCombinations(digits);
+		
+		//[ada, adb, adc, aea, aeb, aec, afa, afb, afc, bda, bdb, bdc, bea, beb, bec, bfa, bfb, bfc, cda, cdb, cdc, cea, ceb, cec, cfa, cfb, cfc]
 		System.out.println(combinations);
 	}
 
