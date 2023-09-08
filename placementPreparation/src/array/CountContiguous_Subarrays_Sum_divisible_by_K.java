1ef98cd08e06aa3c42fa22f0bdd2e2e1adb62dc0
Satyam Singh <satyamsinghkv2@gmail.com>
2023-09-08 23:42:58 +0530

    Count number of contiguous subarrays whose sum is div by K [Map][rem]

--- placementPreparation/src/array/CountContiguous_Subarrays_Sum_divisible_by_K.java
+++ placementPreparation/src/array/CountContiguous_Subarrays_Sum_divisible_by_K.java
@@ -23,25 +23,25 @@ public class CountContiguous_Subarrays_Sum_divisible_by_K {
 	
 	public static int countSubarrays_sum_div_by_k(int[] arr, int k) {
 		int count = 0;
-		int sum = 0;
-		int rem = 0;
+		int sum = 0; //sum till now - basically [Prefix-Sum] 
+		int rem = 0; //remaining value to add 
 		
 		HashMap<Integer, Integer> map = new HashMap<>();
 		map.put(0, 1);
 		
 		for(int i: arr) {
 			sum += i;
-			rem = sum % k;
+			rem = sum % k; //how much value is left to added to it divisible by k
 			if(rem < 0) {
-				rem += k;
+				rem += k; //if remider is negative then add k
 			}
 			
 			if(map.containsKey(rem)) {
-				count += map.get(rem);
-				map.put(rem, map.get(rem)+1);
+				count += map.get(rem); //first add the till now possible subarrays
+				map.put(rem, map.get(rem)+1); //then increase the count of frequency
 			}
 			else {
-				map.put(rem, 1);
+				map.put(rem, 1); //first time reminder coming means it's just the starting of subarray
 			}
 		}
 		return count;
