6cc5af2fef420d7f2261e11bcfa036119153bdc9
Satyam Singh <satyamsinghkv2@gmail.com>
2025-04-06 00:36:01 +0530

    sum of all subsets

--- placementPreparation/src/backtrack_or_recursion/SubsetSum_All.java
+++ placementPreparation/src/backtrack_or_recursion/SubsetSum_All.java
@@ -1,9 +1,24 @@
 package backtrack_or_recursion;
-
+/**
+ * @Subset Sum : Sum of all Subsets
+ * @StriverSheet : https://takeuforward.org/data-structure/subset-sum-sum-of-all-subsets/
+ * Problem : Given an array print all the sum of the subset generated from it, in the increasing order.
+ * 
+ * Input: 
+ * N = 3, 
+ * arr[] = {5,2,1}
+ * 
+ * Output: 0,1,2,3,5,6,7,8
+ * Explanation: We have to find all the subset’s sum and print them.in this case the generated subsets are 
+ *              [ [], [1], [2], [2,1], [5], [5,1], [5,2]. [5,2,1],so the sums we get will be  0,1,2,3,5,6,7,8
+ * -------------------------------------------------------------------------------------------------------------
+ */
 import java.util.*;
 public class SubsetSum_All {
 	
 	//Recursive
+	// TC : O(2^n)
+	// SC : recursive stack
 	public static ArrayList<Integer> sumOfAllsubsets(int[] arr){
 		ArrayList<Integer> res = new ArrayList<>();
 		
