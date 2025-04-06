6e47070c849f59d15d79fa9231e10235297fba38
Satyam Singh <satyamsinghkv2@gmail.com>
2025-04-07 02:19:42 +0530

    Remove Outmost parentheses

---
+++ placementPreparation/src/parentheses/RemoveOutmostParentheses.java
@@ -0,0 +1,32 @@
+package parentheses;
+/**
+ * -------------------------------------------Remove Outmost Parentheses------------------------------------
+ */
+public class RemoveOutmostParentheses {
+	
+	//Approach
+	public static String removeOutmostParentheses(String s){
+		StringBuilder str = new StringBuilder("");
+		int count = 0;
+		
+		for(char ch: s.toCharArray()) {
+			if(ch == '(') {
+				if(count > 0) str.append(ch);
+				count++;
+			}
+			else {
+				count--;
+				
+				if(count > 0) str.append(ch);
+			}
+		}
+		
+		return str.toString();
+	}
+
+	//Driver Code
+	public static void main(String[] args) {
+		System.out.println(removeOutmostParentheses("(()())(())")); //()()()
+	}
+
+}
