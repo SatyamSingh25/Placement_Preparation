64abd6b919a1d813a2b0d5c921bfe82d3e606faa
Satyam Singh <satyamsinghkv2@gmail.com>
2025-04-11 01:26:54 +0530

    largest rectangle in histogram

--- placementPreparation/src/dynamic_Programming/LargestRectangle_in_Histogram.java
+++ placementPreparation/src/dynamic_Programming/LargestRectangle_in_Histogram.java
@@ -2,16 +2,17 @@ package dynamic_Programming;
 /**
  * @author Satyam
  * @Leetcode_84 @Largest_Rectangle_in_Histogram
+ * @Monotonic_Stack
  * Problem statement: Given an array of integers heights representing the histogram's bar height where the width of each bar is 1,
  *                    return the area of the largest rectangle in the histogram.
  *                    
  *            _  
      6      _| |   
-     5     | | |
-     4     | | |  _
-     3  _  | | |_| |
-     2 | |_| | | | | 
-     1 |_|_|_|_|_|_|
+     5     |/|/|
+     4     |/|/|  _
+     3  _  |/|/|_| |
+     2 | |_|/|/| | | 
+     1 |_|_|/|/|_|_|
       0 1 2 3 4 5 6
       
     Input: heights = [2,1,5,6,2,3]
@@ -22,6 +23,7 @@ package dynamic_Programming;
  */
 import java.util.*;
 public class LargestRectangle_in_Histogram {
+	//TC : O(3N)  SC : O(3n)
 	//Largest Rectangle in Histogram
     public static int largestRectangleArea(int[] heights) {
     	
@@ -34,7 +36,7 @@ public class LargestRectangle_in_Histogram {
         int[] rightSmaller = new int[n];
         
         //filling left smallers array
-        for(int i=0; i<n; i++){
+        for(int i=0; i<n; i++){ //O(n)
             while(stack.isEmpty() == false && heights[stack.peek()] >= heights[i]){
                 stack.pop();
             }
@@ -50,7 +52,7 @@ public class LargestRectangle_in_Histogram {
         
         //filling right smaller array
         stack.clear();
-        for(int i=n-1; i>=0; i--){
+        for(int i=n-1; i>=0; i--){//O(n)
             while(stack.isEmpty() == false && heights[stack.peek()] >= heights[i]){
                 stack.pop();
             }
@@ -64,15 +66,46 @@ public class LargestRectangle_in_Histogram {
             stack.push(i);
         }
         //calculating the max rectable area
-        for(int i=0; i<n; i++){
+        for(int i=0; i<n; i++){ //O(n)
             maxArea = Math.max(maxArea, (heights[i] * (rightSmaller[i]-leftSmaller[i]+1)));
         }
+        
+        System.out.println(Arrays.toString(leftSmaller));
+        System.out.println(Arrays.toString(rightSmaller));
         //result
         return maxArea;
     }
+    
+    //Tc : O(n)
+    //Sc : O(n)
+    public static int largestRectangle_in_Histogram(int[] height) {
+    	int n = height.length;
+    	int maxArea = 0;
+    	
+    	Stack<Integer> stack = new Stack<>();
+    	
+    	for(int i=0; i<=n; i++) {
+    		int currHeight = (i==n) ? 0 : height[i]; 
+    		
+    		while(!stack.isEmpty() && currHeight < height[stack.peek()]) {
+    			int maxHeight = height[stack.pop()];
+    			 
+    			int width = (stack.isEmpty()) ? i : i - stack.peek() - 1;
+    			
+    			int currArea = maxHeight * width;
+    			
+    			maxArea = Math.max(maxArea, currArea);
+    		}
+    		stack.push(i);
+    	}
+    	
+    	return maxArea;
+    	
+    }
     //Driver Code
 	public static void main(String[] args) {
-		int[] heights = {2,1,5,6,2,3};
-		System.out.println(largestRectangleArea(heights));
+		int[] heights = {2,1,5,6,3};
+		System.out.println(largestRectangleArea(heights)); 
+		System.out.println(largestRectangle_in_Histogram(heights));
 	}
 }
