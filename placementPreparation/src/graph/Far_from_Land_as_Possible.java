d8f23d54066374543213399609f2812f281527f5
Satyam Singh <satyamsinghkv2@gmail.com>
2025-04-13 20:18:50 +0530

    farthest point from land

--- placementPreparation/src/graph/Far_from_Land_as_Possible.java
+++ placementPreparation/src/graph/Far_from_Land_as_Possible.java
@@ -1,8 +1,23 @@
 package graph;
 /**
- * @Leetcode
+ * @Leetcode_1162
  * @Fathest_0_from_1
  * @BFS
+ * Problem: Given an n x n grid containing only values 0 and 1, 
+ *          where 0 represents water and 1 represents land,
+ *          find a water cell such that its distance to the nearest land cell is maximized, and return the distance. 
+ *          If no land or water exists in the grid, return -1.
+ *          
+ * The distance used in this problem is the Manhattan distance: 
+ *    the distance between two cells (x0, y0) and (x1, y1) is |x0 - x1| + |y0 - y1|.
+ *  
+ * input:-
+ * {1,0,1}            {1,0,0}                 
+ * {0,0,0}            {0,0,0}
+ * {1,0,1}            {0,0,0}
+ * 
+ * Output : 2      O/P: 4  The cell (2, 2) is as far as possible from all the land with distance 4.
+ * The cell (1, 1) is as far as possible from all the land with distance 2. * 
  */
 import java.util.*;
 public class Far_from_Land_as_Possible {
