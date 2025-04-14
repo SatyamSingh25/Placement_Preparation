f2bb16db76339b26b0f55baa8222b5a3e7ed223c
Satyam Singh <satyamsinghkv2@gmail.com>
2025-04-14 06:29:58 +0530

    longest increasing subsequence

--- placementPreparation/src/dynamic_Programming/Longest_Inceasing_Subsequence.java
+++ placementPreparation/src/dynamic_Programming/Longest_Inceasing_Subsequence.java
@@ -1,7 +1,5 @@
 package dynamic_Programming;
 
-import java.util.Arrays;
-
 /**
  * @Contributer-Satyam Singh
  * ----------------------------------------------LEETCODE 300--------------------------------------------------
@@ -17,6 +15,7 @@ import java.util.Arrays;
    
    difficuly = Medium   
  */
+import java.util.*;
 public class Longest_Inceasing_Subsequence {
 	public static int LIS(int[] arr) {
 		int n = arr.length;
@@ -35,8 +34,48 @@ public class Longest_Inceasing_Subsequence {
 		}
 		return finalMax;
 	}
+	
+	//Binary Search 
+	//TC : O(nlog(n))
+	//SC : O(n)
+	public static int LIS_BinarySearch(int[] arr) {
+		List<Integer> result = new ArrayList<>(); //result array
+		
+		for(int num: arr) {
+			int possibleIndex = lowerBound(result, num); //exact place or just greate element index will be returned
+			
+			if(possibleIndex < result.size()) //if place found in existing array
+				result.set(possibleIndex, num);
+			else
+				result.add(num); //add at last
+		}
+		
+		return result.size();
+	}
+	
+	//exact place or just greate element index will be returned
+	//LowerBound -> BinarySearch
+	public static int lowerBound(List<Integer> arr, int target) {
+		int ans = arr.size(); //default at the last
+		int start = 0, end = arr.size()-1;
+		
+		while(start <= end) {
+			int mid = start + (end - start)/2;
+			
+			if(arr.get(mid) >= target) {
+				ans = mid;
+				end = mid - 1;
+			}
+			else {
+				start = mid + 1;
+			}
+		}
+		
+		return ans;
+	}
 	public static void main(String[] args) {
-		int[] arr = new int[] {10,9,2,5,3,7,101,18};
+		int[] arr = new int[] {10,9,2,5,3,7,101,18}; //4
 		System.out.println(LIS(arr));
+		System.out.println(LIS_BinarySearch(arr));
 	}
 }
